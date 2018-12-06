b03397c5b12e51431c9db2bf95a7448e30c1bfe2
Unknown <ludo.07@videotron.ca>
2018-12-05 20:00:05 -0500

    Ajout boutons menu

--- Assets/MenuEffects.cs
+++ Assets/MenuEffects.cs
@@ -6,13 +6,17 @@ public class MenuEffects : MonoBehaviour
 {
     public float speed = 30f;
     public bool monte = false;
-    public float maxSize = 1.5f;
-    public float minSize = 0.5f;
-    public float vitesseChangingSize = 0.01f;
+    public float maxSize = 1.2f;
+    public float minSize = 0.7f;
+    public float vitesseChangingSize = 0.1f;
 
     //TO CHANGE MODE IN EDITOR
     public bool ActivateChangingSize;
     public bool ActivateRotate;
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
     void Update()
     {
         if (ActivateChangingSize)
@@ -39,8 +43,14 @@ public class MenuEffects : MonoBehaviour
             monte = true;
 
         if (monte)
-            transform.localScale += new Vector3(vitesseChangingSize, vitesseChangingSize, vitesseChangingSize);
+            transform.localScale += new Vector3(vitesseChangingSize * Time.deltaTime, vitesseChangingSize * Time.deltaTime, vitesseChangingSize * Time.deltaTime);
         else
-            transform.localScale -= new Vector3(vitesseChangingSize, vitesseChangingSize, vitesseChangingSize);
+            transform.localScale -= new Vector3(vitesseChangingSize * Time.deltaTime, vitesseChangingSize * Time.deltaTime, vitesseChangingSize * Time.deltaTime);
+    }
+
+    IEnumerator waiter()
+    {
+        //Wait for 1 second
+        yield return new WaitForSeconds(1);
     }
 }
